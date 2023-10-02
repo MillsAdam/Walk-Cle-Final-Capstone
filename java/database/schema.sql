b349e650c029df8be4f797103e029aef52537593
Adam Mills <adam.mills.ud@gmail.com>
2023-10-02 15:10:52 -0400

    location data schema added

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -3,6 +3,8 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS locationType;
 DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS rewards;
+DROP TABLE IF EXISTS locationData;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -42,6 +44,19 @@ CREATE TABLE rewards (
     CONSTRAINT PK_rewards_id PRIMARY KEY (rewards_id)
     CONSTRAINT FK_location_type_id FOREIGN KEY (location_type_id) REFERENCES locationType (location_type_id)
     CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE locationData (
+    location_data_id SERIAL,
+    location_data_name varchar (50) NOT NULL UNIQUE,
+    location_data_description varchar (200) NOT NULL,
+    location_data_days varchar (15) NOT NULL,
+    location_data_opening_times varchar (15) NOT NULL,
+    location_data_closing_times varchar (15) NOT NULL,
+    location_data_img_url varchar (200),
+    location_data_info_url varchar (200),
+    CONSTRAINT PK_location_data PRIMARY KEY (location_data_id),
+    CONSTRAINT FK_location_data_name FOREIGN KEY (location_data_name) REFERENCES location (location_name)
 );
 
 
